e62238cf6125c9f1de7851573724b0d8f0b648bd
YuliaOkushko <juli.okushko@gmail.com>
2020-09-02 08:32:13 +0300

    1. Initial settings; 2. Adding routing

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const productRouter = express.Router();
+
+productRouter.use("/create", function (request, response) {
+    response.send("Добавление notes");
+});
+productRouter.use("/update", function (request, response) {
+    response.send("Обновление notes");
+});
+productRouter.use("/delete", function (request, response) {
+    response.send("Удаление notes");
+});
+productRouter.use("/:id", function (request, response) {
+    response.send(`notes ${request.params.id}`);
+});
+productRouter.use("/", function (request, response) {
+    response.send("Список notes");
+});
+
+app.use("/notes", productRouter);
+app.use("/", function (request, response) {
+    response.send("Главная страница"); 
+});
+app.listen(3000);
